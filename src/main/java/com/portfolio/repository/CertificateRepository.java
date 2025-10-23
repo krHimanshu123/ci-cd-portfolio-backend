7235fc9084d5acdefbed766aeae586eeebcb2567
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-10-23 23:01:30 +0530

    Add method to find certificates by display order

--- src/main/java/com/portfolio/repository/CertificateRepository.java
+++ src/main/java/com/portfolio/repository/CertificateRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface CertificateRepository extends JpaRepository<Certificate, Long> {
     List<Certificate> findAllByOrderByDisplayOrderAsc();
+    
 }
