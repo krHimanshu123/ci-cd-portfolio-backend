02c01fae06ad96de7ba8ad049a801c1fa3bc97e5
Himanshu Kumar <162711386+krHimanshu123@users.noreply.github.com>
2025-12-16 23:46:22 +0530

    Add method to retrieve technologies in order

--- src/main/java/com/portfolio/repository/TechnologyRepository.java
+++ src/main/java/com/portfolio/repository/TechnologyRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @Repository
 public interface TechnologyRepository extends JpaRepository<Technology, Long> {
+    
     List<Technology> findAllByOrderByDisplayOrderAsc();
     
 }
